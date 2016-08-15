b97dd844bb49b141408d0a7474442500b97ddf3b
403-ERROR <leonsteiner011098@yahoo.com>
2016-08-15 15:24:02 +0200

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+
+{
+	cout << "   Hello World!\n";
+	cout << "    Leon Here\n";
+	cout << "I Guess this works!\n";
+	int Math = 100;
+	cout << "Number:";
+	Math = Math /12;
+	Math = Math +102;
+	Math = Math -13;
+	Math = Math *1337;
+	
+	cout << Math << endl;
+}
